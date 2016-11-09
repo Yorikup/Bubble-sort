a6368a07ff8b2192d8e9056d35479c61387c2887
Yakov Rogozhin <yorikup198@gmail.com>
2016-11-09 10:58:50 +0300

    bubble representation added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
-sortButton.onclick = function() {
+//$('#myLink').click();
+//sortButton.onclick = function() {
+$('#sortButton').click(sortarray);
+
+function sortarray() {
    var arr = new Array();
+   var numberedBubbles = new Array();
    var count = 10;
+   var x = 70;
+   var tx = 49;
    var div=document.getElementById('sort').innerHTML = '';
    var div1=document.getElementById('sorted').innerHTML = '';
                
    //Генерируем массив целых чисел от 0 до 100 и выводим его
    for (i = 0; i <count; i++)
       arr[i] = Math.floor(Math.random()*(100 - 0 + 1)) + 0;
-   div=document.getElementById('sort');
-   var table="<table><tr>";
+   //div=document.getElementById('sort');
+   //var table="<table><tr>";
    for(i=0; i<arr.length; i++) {
-      table+='<td>'+arr[i]+'</td>';
+      bubble = s.circle(x, 100, 30);
+      bubble.attr({
+        fill: 'lightblue',
+        stroke: 'white',
+        strokeWidth: 5
+      });
+
+      var text = arr[i].toString();
+
+      var number = s.text(tx, 110, text);
+      number.attr({
+         fill: 'white',
+         "font-size": "26px"
+      });
+
+      numberedBubbles[i] = s.g(bubble, number);
+      x += 100;
+      tx += 100;
+      //table+='<td>'+arr[i]+'</td>';
    }
-   table+='</tr></table>'
-   div.innerHTML+=table;
+   //table+='</tr></table>'
+   //div.innerHTML+=table;
                
    //Сортируем массив методом пузырька снова выводим его
    for (var i = 0; i < count; i++)
@@ -30,10 +55,12 @@ sortButton.onclick = function() {
    }
    table1+='</tr></table>'
    div1.innerHTML+=table1;
+
+   move();
 };
 
 var s = Snap("#svg");
-
+/*
 var bubble = s.circle(150, 150, 50);
 bubble.attr({
   fill: 'lightblue',
@@ -48,7 +75,21 @@ number.attr({
 });
 
 var numberedBubble = s.g(bubble, number);
-numberedBubble
+
+var bubble2 = s.circle(280, 150, 50);
+bubble2.attr({
+  fill: 'lightblue',
+  stroke: 'white',
+  strokeWidth: 10
+});
+
+var number2 = s.text(268, 160, "2");
+number2.attr({
+   fill: 'white',
+   "font-size": "40px"
+});
+
+var numberedBubble2 = s.g(bubble2, number2);
 
 //function move(){
    //numberedBubble.animate({cx:150, cy:150}, 200, function(){
@@ -57,7 +98,8 @@ numberedBubble
 //};
 
 function move(){
-   numberedBubble.animate({transform:'translate(100, 0)'}, 700, mina.linear);
+   numberedBubble.animate({transform:'translate(130, 0)'}, 700, mina.linear);
+   numberedBubble2.animate({transform:'translate(-130, 0)'}, 700, mina.linear);
 };
 
-setInterval(move, 3000);
+//setInterval(move, 3000);*/
